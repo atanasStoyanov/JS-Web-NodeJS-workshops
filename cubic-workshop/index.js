53bd293e786c0cb02cdf8c9667d6ba1d98054753
atanasStoyanov <atanass@dxc.com>
2020-05-31 23:03:45 +0300

    feat: add routs to pages and modifiy hbs templated

--- cubic-workshop/index.js
+++ cubic-workshop/index.js
@@ -1,9 +1,11 @@
 const env = process.env.NODE_ENV || 'development';
 
 const config = require('./config/config')[env];
+const expres = require('express');
+const indexRouter = require('./routes');
 const app = require('express')();
 
 require('./config/express')(app);
-require('./config/routes')(app);
 
+app.use('/', indexRouter);
 app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
