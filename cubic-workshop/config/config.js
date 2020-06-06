0c07e15c039bc9f8f39aa54dbaa8a04cc943138d
atanasStoyanov <atanass@dxc.com>
2020-06-06 21:27:45 +0300

    feat:connect with databse

--- cubic-workshop/config/config.js
+++ cubic-workshop/config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     development: {
-        port: process.env.PORT || 3000
+        port: process.env.PORT || 3000,
+        databaseUrl: `mongodb+srv://pesho:${process.env.DB_PASSWORD}@cluster0-xefwn.mongodb.net/cubicle?retryWrites=true&w=majority`
     },
     production: {}
 };
--- cubic-workshop/index.js
+++ cubic-workshop/index.js
@@ -1,9 +1,24 @@
 const env = process.env.NODE_ENV || 'development';
 
+const mongoose = require('mongoose');
 const config = require('./config/config')[env];
 const expres = require('express');
 const indexRouter = require('./routes');
 const app = require('express')();
+
+mongoose.connect(config.databaseUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
+
+    if(err) {
+        console.error(err);
+        throw err;
+    }
+
+    console.log("Database is now setup and running...");
+    
+})
 
 require('./config/express')(app);
 
