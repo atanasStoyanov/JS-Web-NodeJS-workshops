305b817270da9b5298380fa18b9534a950c0fe39
atanasStoyanov <atanass@dxc.com>
2020-06-14 19:45:29 +0300

    fix: delete useless code

--- cubic-workshop/routes/index.js
+++ cubic-workshop/routes/index.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
-const { getAllCubes, getCube, updateCube, getCubeWithAccessories } = require('../controllers/cube');
-const { getAccessories } = require('../controllers/accessory');
+const { getAllCubes } = require('../controllers/cube');
 
 const Cube = require('../models/cube');
 const Accessory = require('../models/accessory');
@@ -22,99 +21,6 @@ router.get('/about', (req, res) => {
         title: 'About | Cube Workshop'
     });
 });
-
-// router.get('/create', (req, res) => {
-//     res.render('create', {
-//         title: 'Create Cube | Cube Workshop'
-//     });
-// });
-
-// router.post('/create', (req, res) => {
-
-//     const {
-//         name,
-//         description,
-//         imageUrl,
-//         difficultyLevel
-//     } = req.body;
-
-//     const cube = new Cube({ name, description, imageUrl, difficulty: difficultyLevel });
-
-//     cube.save((err) => {
-//         if (err) {
-//             console.error(err);
-//             res.redirect('/create');
-//         } else {
-//             res.redirect('/');
-//         }
-//     });
-
-// });
-
-// router.get('/details/:id', async (req, res) => {
-//     const cube = await getCubeWithAccessories(req.params.id);
-
-//     res.render('details', {
-//         title: 'Details | Cube Workshop',
-//         ...cube
-//     });
-// });
-
-// router.get('/create/accessory', (req, res) => {
-//     res.render('createAccessory', {
-//         title: "Create Accessory"
-//     });
-// });
-
-// router.post('/create/accessory', async (req, res) => {
-//     const {
-//         name,
-//         imageUrl,
-//         description
-//     } = req.body;
-
-//     const accessory = new Accessory({ name, imageUrl, description });
-
-//     await accessory.save((err) => {
-//         if (err) {
-//             console.error(err);
-//         } else {
-//             res.redirect('/create/accessory')
-//         }
-//     });
-// });
-
-// router.get('/attach/accessory/:id', async (req, res) => {
-//     const cube = await getCube(req.params.id);
-//     const accessories = await getAccessories();
-
-//     const cubeAccessories = cube.accessories.map(acc => acc._id.valueOf().toString());
-
-//     const notAttchedAccessories = accessories.filter(acc => {
-//         const accessoryString = acc._id.valueOf().toString();
-        
-//         return !cubeAccessories.includes(accessoryString);
-//     });
-
-//     const canAttachAccessory = cube.accessories.length !== accessories.length && accessories.length > 0;
-
-//     res.render('attachAccessory', {
-//         title: 'Attach Accessory',
-//         ...cube,
-//         accessories: notAttchedAccessories,
-//         canAttachAccessory
-//     });
-// });
-
-// router.post('/attach/accessory/:id', async (req, res) => {
-//     const {
-//         accessory
-//     } = req.body;
-
-//     await updateCube(req.params.id, accessory);
-
-//     res.redirect(`/details/${req.params.id}`);
-// });
 
 router.get('*', (req, res) => {
     res.render('404', {
