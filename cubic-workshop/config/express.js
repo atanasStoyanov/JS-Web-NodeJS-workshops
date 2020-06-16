5965835a5760dc463ea1e30d81d7f4654d32a2f6
atanasStoyanov <atanass@dxc.com>
2020-06-17 00:26:07 +0300

    feat:finish signup

--- cubic-workshop/config/express.js
+++ cubic-workshop/config/express.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const cookieParser = require('cookie-parser');
 
 module.exports = (app) => {
+    // app.use(cookieParser);
     app.use(express.json());       // to support JSON-encoded bodies
     app.use(express.urlencoded({extended: true})); // to support URL-encoded bodies
 
